3e42f0c2bfaed9512db521fe66d4d3fae3ddfdba
July Arifianto <hey.archrinto@gmail.com>
2023-03-20 08:51:27 +0700

    add favicon

--- resources/views/layouts/login.blade.php
+++ resources/views/layouts/login.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Warehouse Management System</title>
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
 
     @vite('resources/js/app.js')
     @livewireStyles
